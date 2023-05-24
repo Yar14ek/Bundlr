7f2356ad4afcac471deee99aa2a7113d9d97add7
Olefit <yar14ka@gmail.com>
2023-05-24 17:51:15 +0300

    bundlr

---
+++ src/utils/upload-image.js
@@ -0,0 +1,36 @@
+import fileReaderStream from "filereader-stream";
+import { getBundlr } from "./get-bundlr";
+ 
+/**
+ * Uploads an image to Bundlr.
+ *
+ * @param {*} fileToUpload The file to be uploaded.
+ * @param {*} fileType The mime-type of the file to be uploaded.
+ * @returns
+ */
+export const uploadImage = async (fileToUpload, fileType) => {
+	const bundlr = await getBundlr();
+ 
+	try {
+		const dataStream = fileReaderStream(fileToUpload);
+		const price = await bundlr.getPrice(fileToUpload.size);
+		const balance = await bundlr.getLoadedBalance();
+ 
+		if (price.isGreaterThanOrEqualTo(balance)) {
+			console.log("Funding node.");
+			await bundlr.fund(price);
+		} else {
+			console.log("Funding not needed, balance sufficient.");
+		}
+ 
+		const tx = await bundlr.upload(dataStream, {
+			tags: [{ name: "Content-Type", value: fileType }],
+		});
+ 
+		console.log(`File uploaded ==> https://arweave.net/${tx.id}`);
+ 
+		return "https://arweave.net/" + tx.id;
+	} catch (e) {
+		console.log("Error on upload, ", e);
+	}
+};
